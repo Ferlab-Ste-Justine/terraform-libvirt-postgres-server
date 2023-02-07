17a8171b24577cfb1e21657530a8e0277e656c13
Eric Vallee <eric_vallee@webificservices.com>
2023-02-07 07:40:12 -0500

    feat: INFRA-243 Added support for connecting to libvirt network via name

--- variables.tf
+++ variables.tf
@@ -23,14 +23,16 @@ variable "volume_id" {
 variable "libvirt_network" {
   description = "Parameters of the libvirt network connection if a libvirt network is used. Has the following parameters: network_id, ip, mac"
   type = object({
-      network_id = string
-      ip = string
-      mac = string
+    network_name = string
+    network_id = string
+    ip = string
+    mac = string
   })
   default = {
-      network_id = ""
-      ip = ""
-      mac = ""
+    network_id   = ""
+    network_name = ""
+    ip = ""
+    mac = ""
   }
 }
 
